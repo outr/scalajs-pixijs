5136aed0cc82170c6fc5ed4096e5ad47e8e8dc46
Matt Hicks <matt@outr.com>
2017-04-10 18:02:18 -0500

    Added InteractionData

---
+++ src/main/scala/com/outr/pixijs/PIXI/interaction/InteractionData.scala
@@ -0,0 +1,20 @@
+package com.outr.pixijs.PIXI.interaction
+
+import com.outr.pixijs._
+import org.scalajs.dom.raw.{MouseEvent, TouchEvent}
+
+import scala.scalajs.js
+import scala.scalajs.js.annotation.JSName
+import scala.scalajs.js.|
+
+@js.native
+@JSName("PIXI.interaction.InteractionData")
+class InteractionData extends js.Object {
+  def global: PIXI.Point = js.native
+  def identifier: Double = js.native
+  def originalEvent: MouseEvent | TouchEvent = js.native
+  def target: PIXI.DisplayObject = js.native
+  def getLocalPosition(displayObject: PIXI.DisplayObject,
+                       point: PIXI.Point = js.native,
+                       globalPos: PIXI.Point = js.native): PIXI.Point = js.native
+}
